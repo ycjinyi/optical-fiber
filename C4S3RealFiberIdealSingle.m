4b3bb7c684520fc6717dc34d7fc44295991f0ba2
ycjinyi <liujinyicn@foxmail.com>
2024-03-09 08:14:36 +0800

    adj C4S3

--- C4S3RealFiberIdealSingle.m
+++ C4S3RealFiberIdealSingle.m
@@ -10,7 +10,8 @@ FG = FiberGenerator();
 %设置介质层属性
 OT = OptTool();
 % BL = BiLayer(OT.INR, OT.INI, OT.WNR, OT.WNI, OT.ANR);
-SL = SingleLayer(OT.INR, OT.INI, OT.ANR);
+% SL = SingleLayer(OT.INR, OT.INI, OT.ANR);
+SL = SingleLayer(OT.WNR, OT.WNI, OT.ANR);
 
 %设置波段数据
 % lambdas = 890;
@@ -27,16 +28,16 @@ H3 = (0.1: 0.5: 10.1) * 1e-3;
 %传入不同模型的厚度数据和波段数据进行计算
 OC = OptCompute();
 %flux [a, b, c] 3维,a是波段,b是厚度点,c是接收光纤
-% [flux1, ~] = OC.compute(SL, SPM, R1PM, lambdas, H1, true);
-% [flux2, ~] = OC.compute(SL, SPM, R2PM, lambdas, H2, true);
-% [flux3, ~] = OC.compute(SL, SPM, R3PM, lambdas, H3, true);
-% 
-% %首先需要按照拼接时的关系对计算结果进行拆分,分别将对应接收光纤的光通量累计起来
-% flux1 = sum(flux1(:, :, :), 3);
-% flux2 = sum(flux2(:, :, :), 3);
-% flux3 = sum(flux3(:, :, :), 3);
+[flux1, ~] = OC.compute(SL, SPM, R1PM, lambdas, H1, true);
+[flux2, ~] = OC.compute(SL, SPM, R2PM, lambdas, H2, true);
+[flux3, ~] = OC.compute(SL, SPM, R3PM, lambdas, H3, true);
 
-% save 2024030601.mat R1PM R2PM R3PM SPM SL lambdas H1 H2 H3 flux1 flux2 flux3;
+%首先需要按照拼接时的关系对计算结果进行拆分,分别将对应接收光纤的光通量累计起来
+flux1 = sum(flux1(:, :, :), 3);
+flux2 = sum(flux2(:, :, :), 3);
+flux3 = sum(flux3(:, :, :), 3);
+
+save 2024030901.mat R1PM R2PM R3PM SPM SL lambdas H1 H2 H3 flux1 flux2 flux3;
 
 %颜色表和标签
 CG = ColorGenerator();
@@ -45,7 +46,7 @@ CG = ColorGenerator();
 colorPen = 0.9;
 coff = 9;
 
-load 2024030601.mat;
+load 2024030901.mat;
 
 figure(1);
 for i = 1: size(lambdas, 2)
@@ -55,10 +56,10 @@ end
 grid on;
 legend(lambdaStr);
 title("接收光纤1");
-xlabel("冰厚度(mm)");
+xlabel("水厚度(mm)");
 ylabel("光通量(lm)");
 xlim([0, 10]);
-ylim([0, 1.5* 1e-5]);
+ylim([0, 2* 1e-5]);
 set(gca, "YScale", "log");
 
 figure(2);
@@ -69,10 +70,10 @@ end
 grid on;
 legend(lambdaStr);
 title("接收光纤2");
-xlabel("冰厚度(mm)");
+xlabel("水厚度(mm)");
 ylabel("光通量(lm)");
 xlim([0, 10]);
-ylim([0, 1.5* 1e-5]);
+ylim([0, 2* 1e-5]);
 set(gca, "YScale", "log");
 
 figure(3);
@@ -83,8 +84,8 @@ end
 grid on;
 legend(lambdaStr);
 title("接收光纤3");
-xlabel("冰厚度(mm)");
+xlabel("水厚度(mm)");
 ylabel("光通量(lm)");
 xlim([0, 10]);
-ylim([0, 1.5* 1e-5]);
+ylim([0, 2* 1e-5]);
 set(gca, "YScale", "log");
